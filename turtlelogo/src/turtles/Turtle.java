0d8d1406071546b99cd354c76657b7d3ef6b3b06
James Church <jcchurch@gmail.com>
2018-06-24 15:35:03 -0500

    Adds documentation to Turtle.java

--- turtlelogo/src/turtles/Turtle.java
+++ turtlelogo/src/turtles/Turtle.java
@@ -22,7 +22,12 @@ public final class Turtle {
 
     public Turtle() {
     }
-    
+
+    /**
+     * Initializes the Turtle in the center of a Canvas. Records this location as its home. Throws an IllegalArgumentException if Canvas is null.
+     *
+     * @param canvas the canvas on which we place a turtle.
+     */
     public void initialize(Canvas canvas) {
         if (canvas == null) {
             throw new IllegalArgumentException("The canvas is null.");
@@ -41,40 +46,76 @@ public final class Turtle {
         this.homey = centery;
     }
 
+    /**
+     * Raises the pen (or tail) of this turtle so that it no longer draws to the screen. The default state of the turtle's tail is lowered.
+     */
     public void up() {
         this.drawing = false;
     }
 
+    /**
+     * Lowers the pen (or tail) of this turtle so that it will longer draws to the screen. The default state of the turtle's tail is lowered.
+     */
     public void down() {
         this.drawing = true;
     }
 
+    /**
+     * Prevents the turtle (i.e. the arrow) from being drawn to the screen. The default state is that the arrow is drawn.
+     */
     public void hide() {
         this.showTurtle = false;
     }
 
+    /**
+     * Draws the turtle (i.e. the arrow) on the screen. The default state is that the arrow is drawn.
+     */
     public void show() {
         this.showTurtle = true;
     }
 
+    /**
+     * Moves the turtle to an (x,y) location in the window. The upper-right corner of the screen is location (0, 0). This will draw if the tail is down.
+     *
+     * @param x the x coordinate
+     * @param y the y coordinate
+     */
     public void goTo(double x, double y) {
         context.moveTo(x, y);
         this.assignXandY(x, y);
     }
     
+    /**
+     * Moves the turtle to the home location in the window (i.e. where the turtle started). This will draw if the tail is down.
+     */
     public void goHome() {
         this.goTo(this.homex, this.homey);
     }
 
+    /**
+     * Sets the scale of the turtle. This impacts the line width and the size of the arrow. The default scale is 1.
+     *
+     * @param width the desired line width of the turtle.
+     */
     public void setScale(double width) {
         this.scale = width;
         this.context.setLineWidth(width);
     }
     
+    /**
+     * Sets the color of the turtle. The default color is black.
+     *
+     * @param paint the desired color
+     */
     public void setColor(Paint paint) {
         this.context.setStroke(paint);
     }
 
+    /**
+     * Moves the turtle forward a desired distance.
+     *
+     * @param distance the desired distance
+     */
     public void forward(double distance) {
         double newx = this.x + distance * Math.cos(theta);
         double newy = this.y + distance * Math.sin(theta);
@@ -85,36 +126,66 @@ public final class Turtle {
         }
     }
 
+    /**
+     * Moves the turtle forwards 100 units.
+     */
     public void forward() {
         this.forward(100);
     }
-    
+
+     /**
+     * Moves the turtle backwards a desired distance.
+     *
+     * @param distance the desired distance
+     */
     public void backward(double distance) {
         this.forward(-distance);
     }
     
+    /**
+     * Moves the turtle backwards 100 units.
+     */
     public void backward() {
         this.forward(-100);
     }
 
+    /**
+     * Turns the turtle to the left to a specified degree.
+     *
+     * @param degrees the desired degree to turn.
+     */
     public void left(double degrees) {
         double radians = convertDegreesToRadians(degrees);
         this.theta -= radians;
     }
 
+    /**
+     * Causes the turtle to make a corner turn to the left. Same as saying `left(90)`.
+     */
     public void left() {
         this.left(90);
     }
 
+    /**
+     * Turns the turtle to the right to a specified degree.
+     *
+     * @param degrees the desired degree to turn.
+     */
     public void right(double degrees) {
         double radians = convertDegreesToRadians(degrees);
         this.theta += radians;
     }
 
+    /**
+     * Causes the turtle to make a corner turn to the right. Same as saying `right(90)`.
+     */
     public void right() {
         this.right(90);
     }
 
+    /**
+     * Draws the arrow if the turtle is set to `show`. Otherwise this does nothing. The default state is to draw the turtle.
+     */
     public void drawArrow() {
         if (this.showTurtle) {
             boolean originalPenState = this.drawing;
